8e90f8ec886ba55e897f5d75fb6984e604870d9b
Ben <ben@solero.me>
2020-05-07 17:28:53 +0100

    Use `$POSTGRES_USER` environment variable

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -7,7 +7,7 @@ host="$1"
 shift
 cmd="$@"
   
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGUSER=$POSTGRES_USER PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
